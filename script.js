b1f83c53a1b44e2b36316f46aa51d6894ac835f6
mateoeulORT <48044168@est.ort.edu.ar>
2025-03-24 18:50:00 -0300

    validar envio

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
+document.getElementById("formulario").addEventListener('submit',function(event){
+
+    if (validarLargo() && validarEmail()) {
+ 
+    } 
+    else{   
+        event.preventDefault();
+        let mensaje = document.getElementById('mensaje-envio');
+        mensaje.innerHTML = "Campos incompletos o invalidos"
+    }
+
+})
+
+
 function validarLargo() {
     let ingreso = document.getElementById('nombre').value;
     let mensaje = document.getElementById('mensaje-nombre');
     let inputNombre = document.getElementById('nombre');
 
+
     if (ingreso.length == 0) {
         inputNombre.style.borderColor = "";
         mensaje.innerHTML = ""; 
+        return false
     } 
     else if (ingreso.length <= 3) {
         inputNombre.style.borderColor = "red";  
         mensaje.innerHTML = "Al menos 3 caracteres";
+        return false
     } 
     else {
         inputNombre.style.borderColor = "green"; 
         mensaje.innerHTML = ""; 
+        return true
     }
 }
 
@@ -26,14 +44,17 @@ function validarEmail() {
     {
         mensaje.innerHTML = "";
         emailInput.style.borderColor = "";
+        return false
     } 
     else if (ingreso.includes("@") && ingreso.endsWith(".com")) {   
         emailInput.style.borderColor = "green";
         mensaje.innerHTML = "";
+        return true;
     } 
     else {
         emailInput.style.borderColor = "red";
         mensaje.innerHTML = "Formato no válido (example@gmail.com)";
+        return false
     }
 }
 
