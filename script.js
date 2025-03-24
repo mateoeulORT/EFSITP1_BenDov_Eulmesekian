16858b82429a16038d5403feacd49c36f254808c
mateoeulORT <48044168@est.ort.edu.ar>
2025-03-24 18:18:01 -0300

    validacion nombre y mail

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
 function validarLargo() {
-    let ingreso = document.getElementById('nombre').value
+    let ingreso = document.getElementById('nombre').value;
+    let mensaje = document.getElementById('mensaje-nombre');
+    let inputNombre = document.getElementById('nombre');
 
-    if (ingreso.length <= 3) {
-        document.getElementById('nombre').style.color = "red"
-    } else {
-        document.getElementById('nombre').style.color = "black"
+    if (ingreso.length == 0) {
+        inputNombre.style.borderColor = "";
+        mensaje.innerHTML = ""; 
+    } 
+    else if (ingreso.length <= 3) {
+        inputNombre.style.borderColor = "red";  
+        mensaje.innerHTML = "Al menos 3 caracteres";
+    } 
+    else {
+        inputNombre.style.borderColor = "green"; 
+        mensaje.innerHTML = ""; 
     }
 }
 
 function validarEmail() {
-    let ingreso = document.getElementById('email').value
+    let emailInput = document.getElementById('email');
+    let mensaje = document.getElementById('mensaje-email');
+    let ingreso = emailInput.value;
 
-    if (ingreso) {
-        document.getElementById('nombre').style.color = "red"
-    } else {
-        document.getElementById('nombre').style.color = "black"
+    if (ingreso == "") 
+    {
+        mensaje.innerHTML = "";
+        emailInput.style.borderColor = "";
+    } 
+    else if (ingreso.includes("@") && ingreso.endsWith(".com")) {   
+        emailInput.style.borderColor = "green";
+        mensaje.innerHTML = "";
+    } 
+    else {
+        emailInput.style.borderColor = "red";
+        mensaje.innerHTML = "Formato no válido (example@gmail.com)";
     }
+}
+
+function ValidarContraseña(){
+
+}
+
+function ValidarContraseña2(){
+    
 }
