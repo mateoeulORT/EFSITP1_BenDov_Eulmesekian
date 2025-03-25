c49f1d5c8469e807d165fa3a793b15186c497eb2
CIRO BEN DOV <48111117@ort>
2025-03-25 12:02:24 -0300

    terminado

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.getElementById("formulario").addEventListener('submit',function(event){
 
-    if (validarLargo() && validarEmail()) {
+    let contraseña = validarContraseña()
+
+    if (validarLargo() && validarEmail() && contraseña != "" && validarContraseña2(contraseña)) {
  
     } 
     else{   
@@ -58,10 +60,52 @@ function validarEmail() {
     }
 }
 
-function ValidarContraseña(){
+function validarContraseña(){
+    
+    let ingreso = document.getElementById('contraseña').value;
+    let mensaje = document.getElementById('mensaje-contraseña');
+    let inputContraseña = document.getElementById('contraseña');
 
+
+    if (ingreso.length == 0) {
+        inputContraseña.style.borderColor = "";
+        mensaje.innerHTML = ""; 
+        return false
+        
+    } 
+    else if (ingreso.length >= 8 && /[0-9]/.test(ingreso) && /[a-zA-Z]/.test(ingreso)) {
+        inputContraseña.style.borderColor = "green"; 
+        mensaje.innerHTML = ""; 
+        return ingreso     
+    } 
+    else {
+        inputContraseña.style.borderColor = "red";  
+        mensaje.innerHTML = "Debe contener almenos una letra y un numero (minimo 8 caracteres)"; 
+        return false 
+    }
+    
 }
 
-function ValidarContraseña2(){
-    
+function validarContraseña2(contraseña){
+    let ingreso = document.getElementById('contraseña2').value;
+    let mensaje = document.getElementById('mensaje-contraseña2');
+    let inputContraseña = document.getElementById('contraseña2');
+
+
+    if (ingreso.length == 0) {
+        inputContraseña.style.borderColor = "";
+        mensaje.innerHTML = ""; 
+        return false
+    } 
+    else if (ingreso === contraseña) {
+        inputContraseña.style.borderColor = "green"; 
+        mensaje.innerHTML = ""; 
+        return true
+    } 
+    else {
+        inputContraseña.style.borderColor = "red";  
+        mensaje.innerHTML = "Las contraseñas no coinciden"; 
+        return false  
+    }
+
 }
